dd57bbc1b83e06f4c52f3717544489da91cd7508
diegoalvis <diegoalvispal@gmail.com>
2019-02-19 19:41:56 -0300

    Google fragment created

--- app/src/main/java/com/example/bairestest/GoogleFragment.kt
+++ app/src/main/java/com/example/bairestest/GoogleFragment.kt
@@ -5,11 +5,13 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_google.*
 
 
 class GoogleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        webView.loadUrl("http://www.google.com")
         return inflater.inflate(R.layout.fragment_google, container, false)
     }
 

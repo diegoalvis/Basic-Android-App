d94f99ffadeb6fde32733ca548cfef35641a9f6b
diegoalvis <diegoalvispal@gmail.com>
2019-02-19 19:39:51 -0300

    Init prject

---
+++ app/src/main/java/com/example/bairestest/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.bairestest
+
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private val googleFragment = GoogleFragment.newInstance()
+    private val buttonsFragment = ButtonsFragment.newInstance()
+    private val reposFragment = ReposFragment.newInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.drawer, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_google -> supportFragmentManager.replace(R.id.container, googleFragment)
+            R.id.nav_buttons-> supportFragmentManager.replace(R.id.container, buttonsFragment)
+            R.id.nav_repos -> supportFragmentManager.replace(R.id.container, reposFragment)
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
+
+
+
+fun FragmentManager.replace(container: Int, fragment: Fragment, tag: String? = fragment.tag) { this.beginTransaction().replace(container, fragment).commitAllowingStateLoss() }
